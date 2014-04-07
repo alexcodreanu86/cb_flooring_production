4df455b6a1624789a816bbe2f90b003521434cd2
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-04-07 09:05:08 -0500

    added images to the album show

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   has_attached_file :image_path, 
-                    :styles => { original: '800x1000>'}, 
+                    :styles => { original: '800x1000>', thumb: "100x100>"}, 
                     :default_url => "/images/:style/missing.png"
                     
   validates_attachment :image_path, :content_type => { :content_type => ["image/jpg", "image/jpeg" ,"image/gif", "image/png"] }
