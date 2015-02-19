ccf743dd0c61adcb926fb0bb4c1b9a1c4cb7f021
Alex Codreanu <alexcodreanu86@gmail.com>
2015-02-18 20:45:17 -0600

    add rounded corners and blur out links that get clicked

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -2,16 +2,17 @@
 $(function(){
   /**************** Scroll efect ****************/
 
-  $(document).on("click", ".scroll", function(e){     
+  $(document).on("click", ".scroll", function(e){
     e.preventDefault();
+    this.blur();
     var destination = $(this).attr("href")
     $('html,body').animate({scrollTop:$(destination).offset().top}, 500);
   });
 
   /**************** Galery Modal ****************/
-  
+
   var getDataAndAppendToModal = function(url, modal){
-    $.get(url, function(serverResponse){ 
+    $.get(url, function(serverResponse){
       $(modal).html(serverResponse);
       $(modal).append("<a class='close-reveal-modal'>&#215;</a>");
     });
@@ -19,8 +20,9 @@ $(function(){
 
   $(document).on("click", "#galery-link", function(event){
     event.preventDefault();
+    this.blur();
     var url = $(this).attr("href")
-    getDataAndAppendToModal(url, "#first-modal");    
+    getDataAndAppendToModal(url, "#first-modal");
   })
 
   /**************** Second Modal ****************/
@@ -30,7 +32,7 @@ $(function(){
     var url = $(this).attr("href");
     getDataAndAppendToModal(url,"#second-modal");
   })
-  
+
   $(document).on('opened','#second-modal', function(){
     $(window).trigger('resize')
   });
@@ -42,7 +44,7 @@ $(function(){
     getDataAndAppendToModal(url,"#small-modal");
   });
 
-  
+
   $(document).on("submit",".new_quote_request", function(event){
     event.preventDefault();
     var data  = $(this).serialize();
